66bf8681e29529b6655a7211d47a3d7ec02123f8
ny8nine <nhamilton1@gmail.com>
2017-04-11 22:01:45 -0400

    cleaned up the structure of the HTML file

--- main.js
+++ main.js
@@ -18,42 +18,49 @@
 
 features = {"Park":685,"School":648,"Summit":23,"Populated Place":179,"Locale":376,"Channel":4,"Stream":30,"Military":52,"Island":7,"Bridge":54,"Falls":1,"Cemetery":36,"Building":2039,"Civil":57,"Church":687,"Hospital":101,"Trail":12,"Canal":5,"Tower":31,"Dam":5,"Reservoir":13,"Lake":3,"Cape":12,"Area":17,"Crossing":2,"Valley":5,"Spring":5,"Woods":1,"Bay":2,"Harbor":1,"Beach":2,"Post Office":79,"Airport":19,"Pillar":1,"Gut":1,"Reserve":1}
 
-var colWidth = 30
+var height = d3.max(Object.values(features))/5
+
+var margin = {left: 10, right: 10, top: 10, bottom:10 }
+
+var colWidth = 25
 var colPadding = 4
 var colNumber = Object.keys(features).length
 var whitespace = (colNumber-1)*colPadding
-var space = ((colWidth+colPadding)*colNumber)-colPadding
+var width = ((colWidth+colPadding)*colNumber)-colPadding
+console.log(width, height)
 
 
 var x = d3.scaleBand()
 			.domain(Object.keys(features))
-			.range([0,space])
-			.paddingInner(whitespace/space);
+			.range([0,width])
+			.paddingInner(whitespace/width);
 
 var xAxis = d3.axisBottom(x);
 
- 	var svg = d3.select('body').append('svg')
- 				.attr('height', '100%' )
- 				.attr('width', '100%' )
+var svg = d3.select('#chart').append('svg')
+			.attr('height', '100%' )
+			.attr('width', '100%' )
 
- 	svg.selectAll('rect')
- 		.data(Object.values(features))
- 		.enter().append('rect')
- 	    .attr('x', function(d, i) { return i * (colWidth+colPadding); } )
- 	    .attr('y', function(d, i) { return 500-d/2; })
- 	    .attr('width', colWidth )
- 	    .attr('height', function(d,i){return d/2;})
- 	    .attr('rx', 0)
- 	    .attr('ry', 0)
- 	    .style('fill', '#000');
+var chartGroup = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
- 	svg.append('g')
- 		.attr('class', 'x axis hidden')
- 		.attr('transform', 'translate(' + 0 + ',' + 500 + ')')
- 		.call(xAxis)
- 		.selectAll('text')
- 			.attr("y", 0)
-    		.attr("x", 5)
-			.attr("transform", "rotate(45)")
-			.style("text-anchor", "start");
+chartGroup.selectAll('rect')
+	.data(Object.values(features))
+	.enter().append('rect')
+    .attr('x', function(d, i) { return i * (colWidth+colPadding); } )
+    .attr('y', function(d, i) { return height-d/5; })
+    .attr('width', colWidth )
+    .attr('height', function(d,i){return d/5;})
+    .attr('rx', 0)
+    .attr('ry', 0)
+    .style('fill', '#000');
+
+chartGroup.append('g')
+	.attr('class', 'x axis hidden')
+	.attr('transform', 'translate(' + 0 + ',' + height + ')')
+	.call(xAxis)
+	.selectAll('text')
+		.attr("y", 0)
+		.attr("x", 5)
+		.attr("transform", "rotate(45)")
+		.style("text-anchor", "start");
 
