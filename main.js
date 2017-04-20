40527cc8e8e6bd82a76c6662859d79d66de1dc7f
ny8nine <nhamilton1@gmail.com>
2017-04-19 22:02:25 -0400

    Refactored main.js to be a little cleaner

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
-// var psv = d3.dsvFormat("|");
-// //^created a method that will parse on "|"
-// d3.text("DC_Features_20170201.txt")
-// 	.get(function (error,data) {
-// 		var rows = psv.parse(data)
-// 		var features = {};
+var psv = d3.dsvFormat("|");
+//^created a method that will parse on "|"
+d3.text("DC_Features_20170201.txt")
+	.get(function (error,data) {
+		var rows = psv.parse(data)
+		var features = {};
 
-// 		for (var p = 0; p < rows.length; p++) {
-// 			if(features[rows[p].FEATURE_CLASS]){
-// 				features[rows[p].FEATURE_CLASS] += 1
-// 			}else{
-// 				features[rows[p].FEATURE_CLASS] = 1
-// 			}
-// 		}
-// 		console.log(JSON.stringify(features))
-// 	});
-
-
-features = {"Park":685,"School":648,"Summit":23,"Populated Place":179,"Locale":376,"Channel":4,"Stream":30,"Military":52,"Island":7,"Bridge":54,"Falls":1,"Cemetery":36,"Building":2039,"Civil":57,"Church":687,"Hospital":101,"Trail":12,"Canal":5,"Tower":31,"Dam":5,"Reservoir":13,"Lake":3,"Cape":12,"Area":17,"Crossing":2,"Valley":5,"Spring":5,"Woods":1,"Bay":2,"Harbor":1,"Beach":2,"Post Office":79,"Airport":19,"Pillar":1,"Gut":1,"Reserve":1}
+		for (var p = 0; p < rows.length; p++) {
+			if(features[rows[p].FEATURE_CLASS]){
+				features[rows[p].FEATURE_CLASS] += 1
+			}else{
+				features[rows[p].FEATURE_CLASS] = 1
+			}
+		}
 
 var height = d3.max(Object.values(features))/5
 
 var margin = {left: 10, right: 10, top: 10, bottom:10 }
 
-var colWidth = 25
-var colPadding = 4
+var barWidth = 25
+var barGap = 4
 var colNumber = Object.keys(features).length
-var whitespace = (colNumber-1)*colPadding
-var width = ((colWidth+colPadding)*colNumber)-colPadding
-console.log(width, height)
+var whitespace = (colNumber-1)*barGap
+var width = ((barWidth+barGap)*colNumber)-barGap
+console.log(height, width)
 
 
 var x = d3.scaleBand()
@@ -46,9 +41,9 @@ var chartGroup = svg.append('g').attr('transform', 'translate(' + margin.left + 
 chartGroup.selectAll('rect')
 	.data(Object.values(features))
 	.enter().append('rect')
-    .attr('x', function(d, i) { return i * (colWidth+colPadding); } )
+    .attr('x', function(d, i) { return i * (barWidth+barGap); } )
     .attr('y', function(d, i) { return height-d/5; })
-    .attr('width', colWidth )
+    .attr('width', barWidth )
     .attr('height', function(d,i){return d/5;})
     .attr('rx', 0)
     .attr('ry', 0)
@@ -63,4 +58,6 @@ chartGroup.append('g')
 		.attr("x", 5)
 		.attr("transform", "rotate(45)")
 		.style("text-anchor", "start");
+});
+
 
